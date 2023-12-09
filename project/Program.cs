3853d5574a32815b41b76401d2574ae8263f112c
KLkapelka <153184633+KLkapelka@users.noreply.github.com>
2023-12-09 10:31:31 +0700

    Create Program.cs

---
+++ project/Program.cs
@@ -0,0 +1,25 @@
+﻿// Дано целое положительное число n. Вычислите произведение
+// натуральных чисел, кратных трём и не превышающим число n;
+
+using System;
+class work_4_1
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int n = int.Parse(Console.ReadLine()); 
+        int number = 1; 
+
+        for(int i = 3; i <= n; i += 3)
+        {
+            number *= 3; 
+            if (number >  n) 
+            
+                number /= 3; 
+                break; 
+            }
+        }
+
+        Console.WriteLine($"Получившееся число: {number}"); 
+    }
+} 
